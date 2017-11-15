6ca5cdfa5ed2512c1301c78e0697459539e81ef0
B Terbush <brittanyterbush@gmail.com>
2017-11-15 14:33:20 -0800

    create buttons with event listeners

--- public/newPhoto.js
+++ public/newPhoto.js
@@ -4,6 +4,24 @@ const newImage = () => {
 
 }
 
-const image = document.getElementById("photoItem");
+const upVote = function() {  //arrow function for "this" will not work
+  this.className = "fa fa-thumbs-up";
+  PicId = document.getElementById("photoItem").src.match(/=(\d+)/)[1];
+  console.log(PicId);
+}
 
-image.addEventListener("click", newImage, true);
+const downVote = function() {
+  this.className = "fa fa-thumbs-down"
+
+}
+
+const image = document.getElementById("photoItem");
+const upButton = document.getElementById("upbtn")
+const downButton = document.getElementById("downbtn")
+
+
+image.addEventListener("click", newImage);
+
+
+upButton.addEventListener("click", upVote);
+downButton.addEventListener("click", downVote);
