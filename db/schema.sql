3662dc5d5eb64f8ed956659bf71725eef328c2b0
Niall R Murphy <niallm@gmail.com>
2020-11-09 00:13:02 +0000

    Groups support in the schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS photoVotes;
 CREATE TABLE photoVotes (
   photoID INTEGER PRIMARY KEY,
   upVote INTEGER DEFAULT 0,
-  downVote INTEGER DEFAULT 0
+  downVote INTEGER DEFAULT 0,
+  groups TEXT
 );
