8793e8ef954054f625006d58ecfb906c229c0bfe
Niall R Murphy <niallm@gmail.com>
2020-11-09 00:08:18 +0000

    Initial support for express_sharp

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db/db');
 
+
+const express_sharp = require('express-sharp')
+//import { expressSharp, FsAdapter, HttpAdapter } from 'express-sharp'
+const server_path = 'public'
+
 const app = express();
 
 // need this for bodyParser
@@ -9,14 +14,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // express knows to look for the index.html file in the folder specified.
-app.use(express.static('public'));
+app.use(express.static(server_path));
 
 // express route for POST requests to db
 app.post('/countVote', (req, res) => {
   db.addVote(req.body.chosenPhoto, req.body.thumb, req.body.increasingVote)
-    .then(message => res.json(message))
-    .catch(e => console.log(e));
+  //console.dir(res);
+    //.run(message => res.json(message))
+    //.catch(e => console.log(e));
 });
+
+app.use('/fs-endpoint',
+  express_sharp.expressSharp({
+    imageAdapter: new express_sharp.FsAdapter([server_path].join(__dirname, 'images')),
+  })
+)
 
 const server = app.listen(3000, () => {
   console.log('Listening on port %s...', server.address().port); // string interpolation
