37e0ce0c5c8ccaca8431cb6af40f07427b9d4b69
Niall R Murphy <niallm@gmail.com>
2020-11-09 00:11:47 +0000

    addVote uses sqlite; beginning group processing

--- public/main.js
+++ public/main.js
@@ -1,114 +1,63 @@
-/**
- * Generates a random number between 0-49 for a random image.
- * @return {string} url of random image
- */
-function randomImage() {
-  return `https://picsum.photos/800/500/?image=${Math.round(Math.random() * 49)}`;
-}
+const sqlite3 = require('sqlite3').verbose();
 
-function decide_which_images(group_oriented, display_size) {
-  /**
-   * If group_oriented is false:
-   * Scan the database to find all images, pick display_size random selections
-   * (2 by default).
-   * If group_oriented is true:
-   * Scan the database to find out, for every image, what group(s)
-   * it is in (if any). For example, image 1 in groups [1, 2, 3] and
-   * image 2 in groups [15].
-   * Build a table of all groups, and a mapping between all groups and images.
-   * Randomly select a group (with > display_size members).
-   * Randomly select display_size images within that group.
-   **/
-   //scan_db;
-   if(typeof group_oriented === 'undefined')
-   {
-     
-   }
-   else
-   {
-     // 
-   }
-   if (!group_oriented) {
-     
-   }
-}
+// connects db to app;
 
-/**
- * Sets src of image to a new random image and
- * resets the button visuals (count and outline).
- * @return {undefined}
- */
-function newImage() {
-  this.src = randomImage();
-  const up = document.getElementById('upbtn');
-  const down = document.getElementById('downbtn');
-  up.classList.add('fa-thumbs-o-up');
-  up.classList.remove('fa-thumbs-up');
-  down.classList.add('fa-thumbs-o-down');
-  down.classList.remove('fa-thumbs-down');
-  up.innerHTML = '  ';
-  down.innerHTML = '  ';
-}
-
-/**
- * Determine whether we are adding a vote or removing it
- * @param  {object} element The html button logging the vote
- * @return {boolean} true if vote is being added to count, false if being removed
- */
-function addingOrRemoving(element) {
-  if(element.classList.contains('fa-thumbs-o-up') || element.classList.contains('fa-thumbs-o-down')){
-    return true;
+let db = new sqlite3.Database('./pics.db', (err) => {
+  if (err) {
+    console.error(err.message);
   }
-  return false;
-}
+  console.log('Connected to the pics database.');
+});
 
 /**
- * Indicates vote on the DOM, calls fetch to log vote
- * and get back total votes counts, updates DOM accordingly.
- * @return {undefined}
+ * Queries the database to (add/remove) an (up/down) vote on a given photo.
+ * @param {number} photo  ID of the photo being voted on
+ * @param {string} thumb  'up' if upvote 'down' if downvote
+ * @param {boolean} adding true if adding vote, false if removing
+ * @returns {Promise} Resolving to object representing the picture with keys upvote and downvote
  */
-function vote() { // arrow function for 'this' will not work
-  const PicId = document.getElementById('photoItem').src.match(/=(\d+)/)[1];
-  const thumb = /(.+)btn/.exec(this.id)[1]; // thumb up or thumb down?
-  const increasingVote = addingOrRemoving(this);
+const addVote = (photo, thumb, adding) => {
+  const add = adding ? 1 : -1;
 
-  if (increasingVote) {
-    this.classList.add(`fa-thumbs-${thumb}`);
-    this.classList.remove(`fa-thumbs-o-${thumb}`);
-  } else {
-    this.classList.add(`fa-thumbs-o-${thumb}`);
-    this.classList.remove(`fa-thumbs-${thumb}`);
-  }
-
-  fetch('/countVote', {
-    method: 'POST',
-    // tells the route the body is in json so we can get params from it.
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ chosenPhoto: PicId, thumb, increasingVote }),
-  }).then(dataStream => dataStream.json())
-    .then((message) => {
-      document.getElementById('upbtn').innerHTML = message.upvote;
-      document.getElementById('downbtn').innerHTML = message.downvote;
-    }).catch((error) => {
-      console.log(error);
+  let sql = `UPDATE
+    photoVotes
+  SET
+    ${thumb}Vote = ${thumb}Vote + ${add}
+  WHERE
+    photoID = $1`;
+  console.log(`${sql}`)
+  db.serialize(() => {
+    db.run(sql, photo, function(err) {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log(`Row(s) updated: ${this.changes}`);
     });
-}
-
-/**
- * Sets src of the image to a new random image
- * @return {undefined}
- */
-window.onload = () => {
-  document.getElementById('photoItem').src = randomImage();
+    return db;
+  });
 };
 
-const image = document.getElementById('photoItem');
-const upButton = document.getElementById('upbtn');
-const downButton = document.getElementById('downbtn');
+// close the database connection
+function cleanup() {
+  //db.close((err) => {
+  //  if (err) {
+  //    return console.error(err.message);
+  //  }
+  //};
+};
 
-/* event listeners for image */
-image.addEventListener('click', newImage);
+const buildImageStructure = () => {
+  let sql = 'SELECT * FROM photoVotes';
+  db.serialize(() => {
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      rows.forEach((row) => {
+        console.log(row.name);
+      });
+    });
+  });
+}
 
-/* event listeners for buttons */
-upButton.addEventListener('click', vote);
-downButton.addEventListener('click', vote);
+module.exports = { addVote, buildImageStructure };
