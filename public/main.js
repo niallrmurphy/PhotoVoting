f86fc7c844d85a0b934ce74f25e64b8f3c9345f2
Niall Richard Murphy <niallm@gmail.com>
2020-11-29 01:03:13 +0000

    Remove stuff we're no longer doing, or doing better elsewhere with more equipment.

--- public/main.js
+++ public/main.js
@@ -1,47 +1,10 @@
-/**
- * Generates a random number between 0-49 for a random image.
- * @return {string} url of random image
- */
-function randomImage() {
-  return `https://picsum.photos/800/500/?image=${Math.round(Math.random() * 49)}`;
-}
-
-function decideWhichImages(group_oriented, display_size) {
-  /**
-   * If group_oriented is false:
-   * Scan the database to find all images, pick display_size random selections
-   * (2 by default).
-   * If group_oriented is true:
-   * Scan the database to find out, for every image, what group(s)
-   * it is in (if any). For example, image 1 in groups [1, 2, 3] and
-   * image 2 in groups [15].
-   * Build a table of all groups, and a mapping between all groups and images.
-   * Randomly select a group (with > display_size members).
-   * Randomly select display_size images within that group.
-   **/
-  // Things we'll need no matter what
-  if(typeof display_size === 'undefined') {
-    display_size = 2;
-  }
-  console.log(display_size);
-  countImages();
-  createPhotoArray();
-  if(typeof group_oriented === 'undefined')
-  {
-    _.sample(total_photo_array, display_size);
-  }
-  else
-  {
-    //
-  }
-}
 
 /**
  * Sets src of image to a new random image and
  * resets the button visuals (count and outline).
  * @return {undefined}
  */
-function newImage() {
+function newPage() {
   this.src = randomImage();
   const up = document.getElementById('upbtn');
   const down = document.getElementById('downbtn');
